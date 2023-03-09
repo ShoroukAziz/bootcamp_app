53107203f04cafe6209761172aa85411d894ec7f
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-08 23:20:17 -0700

    parameterize values

--- teachers.js
+++ teachers.js
@@ -9,15 +9,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}`];
+
+queryString = `
+SELECT DISTINCT teachers.name as teacher  , cohorts.name as cohort
 FROM assistance_requests
 JOIN students on students.id = assistance_requests.student_id
 JOIN teachers on teachers.id = assistance_requests.teacher_id
-JOIN cohorts on cohorts.id = students.cohort_id 
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+JOIN cohorts on cohorts.id  = students.cohort_id 
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
